dd7f47ca620f48fe88075d15105e350bfbe694b3
jeminsieow <sjemin7@gmail.com>
2021-07-27 16:30:11 +0800

    Add Fisher-Yates shuffle algorithm to shuffle array

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import { COLOR_DARK_GREY } from './utilities/constants';
 
 const Container = styled.View`
   flex: 1;
-  background-color: #39353C;
+  background-color: #39353c;
   padding: 20px;
 `;
 
@@ -17,7 +17,7 @@ const Header = styled.View`
 
 const RestartButton = styled.Text`
   font-size: 20px;
-  color: #1B98F2;
+  color: #1b98f2;
 `;
 
 const StepsText = styled.Text`
@@ -25,14 +25,46 @@ const StepsText = styled.Text`
   color: white;
 `;
 
+const CARD_PAIRS_VALUE = [1, 2, 3, 4, 5, 6];
+
+// Fisher-Yates shuffle algorithm
+function shuffleCards(array) {
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  console.log(array);
+  return array;
+}
+
 export default function Main() {
+  const [cards, setCards] = useState(
+    shuffleCards.bind(null, CARD_PAIRS_VALUE.concat(CARD_PAIRS_VALUE))
+  );
+
   return (
     <Container>
       <Header>
-        <RestartButton>Restart</RestartButton>
+        <TouchableOpacity
+          onPress={() =>
+            setCards(
+              shuffleCards.bind(null, CARD_PAIRS_VALUE.concat(CARD_PAIRS_VALUE))
+            )
+          }
+        >
+          <RestartButton>Restart</RestartButton>
+        </TouchableOpacity>
         <StepsText>STEPS:</StepsText>
       </Header>
-      
+
+      {cards.map((card, i) => {
+        return <Text key={i}>{card}</Text>;
+      })}
+
       <StatusBar style="auto" />
     </Container>
   );
